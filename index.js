b25c98e62a5db4c3830359321dc12845758a0523
Jen <jen.lijo@zalando.de>
2018-03-10 21:03:44 +0100

    Render all articles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const NewsService = require('./controllers/news-service');
 
 const Sources = require('./models/sources');
@@ -8,18 +9,17 @@ const News = require('./models/news');
 const sources = new Sources();
 const news = new News(sources);
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res, next) => {
-  res.sendFile(__dirname + '/views/index.html');
-  // res.send('Hello!');
+  res.render('index');
 });
 
-app.get('/all', async (req, res, next) => {
-  res.send(await NewsService.findAll());
+app.get('/article/all', async (req, res, next) => {
+  const articles = await NewsService.findAll();
+  res.render('article', { articles })
 });
-
-
 
 app.listen(3030, () => {
   console.log('Server listening...');
 });
-
