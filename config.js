b4aa9b4a82c814af4afef888a8eed45635d47eb6
Jen <jen.lijo@zalando.de>
2018-05-01 11:49:44 +0200

    Remove unused files

--- config.js
+++
@@ -1,6 +0,0 @@
-// const NewsAPI = require('newsapi');
-// const KEYS = require('./keys.js') || '';
-
-// const API = new NewsAPI(process.env.NEWS_API_KEY || KEYS.NEWS_API);
-
-// module.exports = API;
